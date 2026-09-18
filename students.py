c22fbc71820de7002c64a6f08842d3c6af091087
Parker Millsap <parkermillsap28@gmail.com>
2026-09-18 10:13:13 -0500

    Add files via upload

--- students.py
+++ students.py
@@ -3,9 +3,8 @@ students = {}
 while 1:
     print("1 Add a student")
     print("2 Remove a student")
-    print("3 Replace courses")
-    print("4 Display course list")
-    print("5 Exit")
+    print("3 Edit a student")
+    print("4 Display students")
     choice = input("Please enter your choice: ")
 
     if choice == "1":
@@ -22,21 +21,19 @@ while 1:
         print("student has been added")
     elif choice == "2":
         del students[input("Please enter s#: ")]
+        print("student has been deleted")
     elif choice == "3":
-        students[input("Please enter s#: ")] = (
-            while 1:
-                input("what info would you like to change?"
-                      "1 Name"
-                      "2 Major"
-                      "3 Year")
-                choice = input("Please enter your choice: ")
-                if choice == "1":
-                    name = input("Please enter student name: ")
-                elif choice == "2":
-                    major = input("Please enter major: ")
-                elif choice == "3":
-                    year = input("Please enter year: ")
-                else:
-                    print("Please enter a valid choice"))
-
-    print("Student has been updated")
+        s_id = input("Please enter s#: ")
+        if s_id in students:
+            students[s_id]["stu_name"] = input("Please enter new name: ")
+            students[s_id]["stu_major"] = input("Please enter new major: ")
+            students[s_id]["stu_year"] = input("Please enter new year: ")
+        else:
+            print("Student not found.")
+        print("Student has been updated")
+    elif choice == "4":
+        print (students)
+    elif choice == "5":
+        exit()
+    else:
+        print("Please enter a valid option")
